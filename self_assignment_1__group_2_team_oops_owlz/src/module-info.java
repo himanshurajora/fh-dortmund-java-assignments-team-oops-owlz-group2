100a3e3aaa2616f9302ffedf2050cd6cab0dca6c
Himanshu Jangid <himanshujhar@gmail.com>
2025-09-17 23:02:25 +0530

    initial commit

---
+++ self_assignment_1__group_2_team_oops_owlz/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module self_assignment_1__group_2_team_oops_owlz {
+}
---
+++ self_assignment_1__group_2_team_oops_owlz/src/self_assignment_1__group_2_team_oops_owlz/HelloWorld.java
@@ -0,0 +1,9 @@
+package self_assignment_1__group_2_team_oops_owlz;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// The classic hello world program
+		System.out.println("Hello World");
+	}
+}
